448b740d622d6bb93ed72c693d4da1065480b9a4
Omran Jamal <o.jamal97@gmail.com>
2025-08-07 22:06:18 +0600

    init

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <h1 className="p-10 font-bold text-xl font-mono text-slate-600">
+        live long and prosper
+      </h1>
+    </>
+  )
+}
+
+export default App
